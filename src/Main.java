f88a0b4e223f60a58c3b0e3087d6ad1078587333
Trofimov Nikita <trofimov1nv@gmail.com>
2020-11-01 13:07:03 +0300

    Added the ternary operator

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ public class Main {
         int balance = 100;
         int amount = 1100;
         int bonus;
-        if (amount < 1000) {
+/*        if (amount < 1000) {
             bonus = 0;
         } else {
             bonus = amount / 100;
-        }
+        }*/
+
+        bonus = (amount < 1000 ) ? 0 : amount / 100; //Оформил через тернарный оператор
+
         System.out.printf("You bonus: ");
         System.out.println(bonus);
+
         balance = balance + amount + bonus;
         System.out.printf("You balance: ");
         System.out.println(balance);
